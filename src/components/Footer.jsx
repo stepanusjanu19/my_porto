5a73302bea68936e90749f37e25bb010e77c622c
stepanusjanu19 <kitoyu22@gmail.com>
2024-04-01 18:02:36 +0700

    build myporto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        &copy; 2021. Made with ❤️ by{" "}
+        <a href="https://github.com/stepanusjanu19">stepanusjanu19</a>.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
